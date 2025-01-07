c8cce1ea8be404cb6a368ca13e64536ab8b2ff7a
Humbert Dominguez <humbertodelac13@gmail.com>
2025-01-06 21:02:31 -0600

    build corregido

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tailmotion_web/tree/deploy-branch/'
+  base: '/tailmotion_web/',
+  build: {
+    outDir: 'docs',
+  },
 })
